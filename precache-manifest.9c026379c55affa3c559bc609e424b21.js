653aac11451a752aefbdd90e27055b11a14732c3
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2021-12-09 15:12:54 +0530

    Updates

---
+++ precache-manifest.9c026379c55affa3c559bc609e424b21.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d45b2cc689e1162975162d1336096e4",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "3a5312e1e69feae9101c",
+    "url": "/react-deploy/static/js/2.0d0954be.chunk.js"
+  },
+  {
+    "revision": "0c5b105e1be74f1d3dbf",
+    "url": "/react-deploy/static/js/main.29b273ab.chunk.js"
+  },
+  {
+    "revision": "66496698585c6a180ec4",
+    "url": "/react-deploy/static/js/runtime-main.b012efad.js"
+  }
+]);
