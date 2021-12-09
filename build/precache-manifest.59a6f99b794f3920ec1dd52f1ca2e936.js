93ff612add7f01cedf7913ddf79dfd54700b5ec6
Avinash Kushwaha <avinashkushwaha2001@gmail.com>
2021-12-09 15:12:25 +0530

    Font changes

---
+++ build/precache-manifest.59a6f99b794f3920ec1dd52f1ca2e936.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e897b4dfcd84a204c87ce56e998311",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "3a5312e1e69feae9101c",
+    "url": "/react-deploy/static/js/2.0d0954be.chunk.js"
+  },
+  {
+    "revision": "0c5b105e1be74f1d3dbf",
+    "url": "/react-deploy/static/js/main.29b273ab.chunk.js"
+  },
+  {
+    "revision": "66496698585c6a180ec4",
+    "url": "/react-deploy/static/js/runtime-main.b012efad.js"
+  }
+]);
